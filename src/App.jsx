af7ec0e97a4f9713014bd1025f93f3a46c9f3750
colt <drownworb@gmail.com>
2024-11-05 20:05:02 +0800

    add handling events and using two-way binding

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import { useState } from "react";
 import Header from "./components/Header";
 
 import "./App.css";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App;
